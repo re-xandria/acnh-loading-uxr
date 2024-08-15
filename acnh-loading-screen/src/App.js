5f003117da44f6c6a32eb12118b566295896f9ce
Xandria Crosland <xandria.crosland@gmail.com>
2024-08-15 13:51:59 -0400

    render scene a and b

--- acnh-loading-screen/src/App.js
+++ acnh-loading-screen/src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useRef } from "react";
 import "./Components/Button.js";
 import "./Components/Scene.js";
 import "./App.scss";
@@ -7,7 +7,6 @@ import Scene from "./Components/Scene.js";
 
 export default function App() {
   const [renderScene1, setRenderScene1] = useState(true);
-
   const scene1 = "Loading Screen A";
   const scene2 = "Loading Screen B";
 
@@ -16,20 +15,19 @@ export default function App() {
     document.querySelector(".buttonA").classList.toggle("change")
     document.querySelector(".buttonB").classList.toggle("change")
   }
-
+  
   return (
     <div>
-      {/* Make reusable button component that handles onClick and changes scenes*/}
       <div className="button-container">
         <div className="menu-button change" onClick={(e) => toggleMenu(e.currentTarget)}>
-          <div className="bar1"></div>
-          <div className="bar2"></div>
-          <div className="bar3"></div>
+          <div className="bar1 change"></div>
+          <div className="bar2 change"></div>
+          <div className="bar3 change"></div>
         </div>
         <Button className="buttonA" onClick={() => setRenderScene1(true)} scene={scene1} />
         <Button className="buttonB" onClick={() => setRenderScene1(false)} scene={scene2} />
       </div>
-      <Scene className='scene' renderScene1={renderScene1} />
+      <Scene className='scene' renderScene1={renderScene1}  />
     </div>
   );
 }
--- acnh-loading-screen/src/Components/Scene.js
+++ acnh-loading-screen/src/Components/Scene.js
@@ -1,11 +1,14 @@
 import Spline from "@splinetool/react-spline";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 const Scene = ({ renderScene1 }) => {
   const [text, setText] = useState(
     "Tips and information that will help you on your adventure will appear here!"
   );
   const [id, setId] = useState(Math.floor(Math.random() * 100));
+  const [isLoadedA, setIsLoadedA] = useState(false);
+  const [isLoadedB, setIsLoadedB] = useState(false);
+  const [objectToAnimate, setObjectToAnimate] = useState(null);
 
   //use math.random to return a random id, cannot be the same as the current id
   //function needs to be called on every mouse down event
@@ -26,61 +29,91 @@ const Scene = ({ renderScene1 }) => {
       .catch((error) => console.log("Error retrieving text:", error));
   };
 
-  useEffect(() => {
-    // Initial fetch
+  const handleAnimationEvent = (e) => {
+    console.log(e, "Animation ended");
+    setId(Math.floor(Math.random() * 100));
     getTip();
+  };
 
-    // Set interval to fetch tips every 4 seconds
-    const intervalId = setInterval(() => {
-      setId(Math.floor(Math.random() * 100));
-      console.log(id)
-      getTip();
-    }, 3000);
+  useEffect(() => {
+    setIsLoadedA(false);
+    setIsLoadedB(false);
+  }, [renderScene1]);
 
-    // Cleanup interval on component unmount
-    return () => {
-      clearInterval(intervalId);
-      console.log("Interval cleared");
-    };
-  }, [id]);
+  const handleLoadedA = () => {
+    setIsLoadedA(true);
+  };
+
+  const handleLoadedB = (spline) => {
+    setIsLoadedB(true);
+    setObjectToAnimate(
+      spline.findObjectById("05a4c250-ca69-41b7-8c65-e301b3b11e6f")
+    );
+  };
+
+  // Handler for screen click
+  const handleScreenClick = () => {
+    if (objectToAnimate) {
+      console.log(objectToAnimate);
+
+      // Trigger animation in Spline
+      objectToAnimate.emitEvent("mouseDown");
+      console.log("going");
+    }
+  };
 
   return (
     <div className="scene">
       {renderScene1 ? (
         <div className="SA">
-          <Spline scene="https://prod.spline.design/Z0NNAiT7tCuqSR5Q/scene.splinecode"></Spline>
-          <div className="loadingA">
-            <span>L</span>
-            <span>O</span>
-            <span>A</span>
-            <span>D</span>
-            <span>I</span>
-            <span>N</span>
-            <span>G</span>
-            <span>.</span>
-            <span>.</span>
-            <span>.</span>
-          </div>
+          <Spline
+            scene="https://prod.spline.design/Z0NNAiT7tCuqSR5Q/scene.splinecode"
+            onLoad={handleLoadedA}
+          ></Spline>
+          {isLoadedA && (
+            <div className="loadingA">
+              <span>L</span>
+              <span>O</span>
+              <span>A</span>
+              <span>D</span>
+              <span>I</span>
+              <span>N</span>
+              <span>G</span>
+              <span>.</span>
+              <span>.</span>
+              <span>.</span>
+            </div>
+          )}
         </div>
       ) : (
         <div className="SB">
           <Spline
             className="SB-obj"
             scene="https://prod.spline.design/xsGg0p2xv04ACpvW/scene.splinecode"
+            onLoad={handleLoadedB}
           ></Spline>
-          <p className="tips">{text}</p>
-          <div className="loadingB">
-            <span>L</span>
-            <span>O</span>
-            <span>A</span>
-            <span>D</span>
-            <span>I</span>
-            <span>N</span>
-            <span>G</span>
-            <span>.</span>
-            <span>.</span>
-            <span>.</span>
-          </div>
+          {isLoadedB && (
+            <div>
+              <button type="button" onClick={handleScreenClick}>
+                Trigger Spline Animation
+              </button>
+              <p onAnimationIteration={handleAnimationEvent} className="tips">
+                {text}
+              </p>
+              <div className="loadingB">
+                <span>L</span>
+                <span>O</span>
+                <span>A</span>
+                <span>D</span>
+                <span>I</span>
+                <span>N</span>
+                <span>G</span>
+                <span>.</span>
+                <span>.</span>
+                <span>.</span>
+              </div>
+            </div>
+          )}
         </div>
       )}
     </div>
